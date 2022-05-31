844cd0ec3fccd56808d0b0f03549c8558b460848
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-05-31 11:45:20 +0600

    design navMneu

---
+++ src/components/home/NavMenu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavMenu = () => {
+  return (
+    <div className='navContainer'>
+      <ul className='navItems'>
+        <li><a href='#'>Groups</a></li>
+        <li><a href='#'>Projects</a></li>
+        <li><a href='#'>Publications</a></li>
+        <li><a href='#'>Centers</a></li>
+        <li><a href='#'>Initiatives</a></li>
+
+      </ul>
+
+    </div>
+  )
+}
+
+export default NavMenu
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import NavMenu from '../components/home/NavMenu'
 import TopBanner from '../components/home/TopBanner'
 
 
 const HomePage = () => {
   return (
-    <TopBanner/>
+    <>
+        <TopBanner/>
+    <NavMenu/>
+    </>
+
   )
 }
 
