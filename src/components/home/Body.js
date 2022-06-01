043bdbed490276f575965bbb7ea190c79438a279
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-01 12:20:17 +0600

    design body section

--- src/components/home/Body.js
+++ src/components/home/Body.js
@@ -1,31 +1,277 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 import Logo from '../../assets/images/logo.jpg'
 const Body = () => {
-  return (
-    <div className='cardContainer'>
-        <div className='singleCard'>
-            <div className='topSection'>
-                <img src={Logo} />
-                <h2>affective Computing</h2>
-            </div>
-            <div className='middleSection'>
-                <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
-            </div>
-            <div className='bottomSection'>
-                <h5>Rafiqul Islam</h5>
-                <ul className='tagLists'>
-                    <li><a href='#'>#Tag1</a></li>
-                    <li><a href='#'>#Tag2</a></li>
-                    <li><a href='#'>#Tag3</a></li>
-                    <li><a href='#'>#Tag4</a></li>
-                </ul>
+    return (
+        <div className='cardContainer'>
+            <Grid container>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4}md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={4} md={4} lg={3}>
+                    <div className='singleCard'>
+                        <div className='topSection'>
+                            <img src={Logo} />
+                            <h2>affective Computing</h2>
+                        </div>
+                        <div className='middleSection'>
+                            <h2>Advancing human wellbeing by developing new ways to communicate, understand, and respond to emotion</h2>
+                        </div>
+                        <div className='bottomSection'>
+                            <h5>Rafiqul Islam</h5>
+                            <ul className='tagLists'>
+                                <li><a href='#'>#Tag1</a></li>
+                                <li><a href='#'>#Tag2</a></li>
+                                <li><a href='#'>#Tag3</a></li>
+                                <li><a href='#'>#Tag4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Grid>
 
-            </div>
+            </Grid>
+
+             
 
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Body
